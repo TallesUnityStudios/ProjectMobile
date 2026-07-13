7bd8f42d69a17d8d5c8eb460db32823c69ab604f
TallesUnityStudios <tallesrosini.2018@gmail.com>
2026-07-13 20:02:40 -0300

    add new pack from PowerUP

---
+++ Assets/Scripts/PowerUps/PowerUpBase.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpBase : ItemCollectableBase
+{
+    [Header("PowerUp")]
+    public float duration;
+
+    protected override void OnCollect()
+    {
+        base.OnCollect();
+        StartPowerUp();
+    }
+
+    protected virtual void StartPowerUp()
+    {
+        Debug.Log("Power-up started!");
+        Invoke(nameof(EndPowerUp), duration);
+    }
+
+    protected virtual void EndPowerUp()
+    {
+        Debug.Log("Power-up ended!");
+    }
+}
